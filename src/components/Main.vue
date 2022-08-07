0f23a6e5d57f3b9a536a1fb239fac81d134569cb
weifan <weifan4569908@gmail.com>
2022-08-07 12:00:15 +0800

    v2.12

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -70,7 +70,7 @@
                                 </h1>
                             </div>
                             <div class="wrap-card-content-block-text">
-                                <a href="/files/募款說明.pdf" download>
+                                <a href="/files/2022清交畢業歌NFT募資活動-募款說明.pdf" download>
                                     企劃書連結
                                 </a>
                             </div>
